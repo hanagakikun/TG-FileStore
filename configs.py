1ad1736eefbd774d1b97ecf3555bf83a9d918c8f
hanagakikun <sansumabaro046@gmail.com>
2024-10-04 19:15:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
+	API_ID = int(os.environ.get("13084577", "0"))
+	API_HASH = os.environ.get("1c0b20897a5fd2a674db1fa45d4c1a96")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	BOT_USERNAME = os.environ.get("Hanagaki_File_Stream_Bot")
+	DB_CHANNEL = int(os.environ.get("-1002474217593", "-100"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1298304662"))
+	DATABASE_URL = os.environ.get("mongodb+srv://sansumabaro046:15LxLs6Z2Ffm2xFk@cluster0.7pscq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002120845917")
+	LOG_CHANNEL = os.environ.get("-1002474217593L")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
